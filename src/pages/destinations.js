94f091d622c50dbca3ee2cc907125846c6806c40
Carlos <carlos_gomes1809@hotmail.com>
2021-02-10 00:30:42 -0300

    final project

---
+++ src/pages/destinations.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Destinations = () => (
+  <Layout>
+    <SEO title="Destinations" />
+    <h1 className="destinations">Destinations</h1>
+  </Layout>
+)
+
+export default Destinations
